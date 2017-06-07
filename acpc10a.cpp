34bd856d7e7d4b75403391832b819b3340224326
rajat_gautam <gautam.rajat34@gmail.com>
2017-06-08 03:41:25 +0530

    Logic

---
+++ acpc10a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b,c;
+	while(1) {
+		scanf("%d %d %d",&a,&b,&c);
+		if(a==0&&b==0&&c==0) {
+			break;
+		}
+		if(c-b==b-a) {
+			printf("AP %d\n",c+b-a);
+		}
+		else {
+			printf("GP %d\n",c*(b/a));	
+		}
+	}
+	return 0;
+}
