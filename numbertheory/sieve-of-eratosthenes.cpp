8b7164fd8e1b06a186c70144d0334f62900b107e
rajat_gautam <gautam.rajat34@gmail.com>
2017-06-16 15:55:26 +0530

    Time Complexity : O(n)

---
+++ numbertheory/sieve-of-eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> prime;
+#define MAX 100000
+void sieve() {
+	vector<bool> sieve(MAX+1);
+	fill(sieve.begin(),sieve.end(),true);
+	for(int i=2;i*i<MAX;i++) {
+		if(sieve[i]) {
+			for(int j=i*i;j<MAX;j=j+i) {
+				sieve[j]=false;
+			}
+		}
+	}
+	for(int i=2;i<MAX;i++) {
+		if(sieve[i])
+			prime.push_back(i);
+	}	
+}	
+int main() {
+	sieve();
+	for(int i=0;i<prime.size();i++) {
+		printf("%d ",prime[i]);
+	}
+	printf("A");
+	return 0;
+}
