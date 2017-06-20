26f52bb97c345f141810210db36d5205764279e0
Rajat Gautam <GAUTAM.RAJAT34@GMAIL.COM>
2017-06-20 20:22:21 +0530

    Added few basics

---
+++ bit_mask/Tricks.cpp
@@ -0,0 +1,11 @@
+Used in finding duplicates
+  A^0=A
+  A^A=A
+  A^(B^C)=(A^B)^C
+
+General Property
+  Check if power of 2 : (x)&(x-1)==0
+  Set the i'th bit : b|(1<<i)
+  Unset the i'th bit : b&!(1<<i)
+  Check if i'th bit is set or not : b&(1<<i)
+  Bit mask : Generate all sub array.
